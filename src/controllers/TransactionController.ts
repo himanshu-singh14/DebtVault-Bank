4be8b70844a3177c9b1bc872871a041315aeefb5
l-limanshu-Singh <himanshusingh1471998@gmail.com>
2023-08-18 08:57:12 +0530

    Added API endpoint for withdrawing money

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -4,13 +4,27 @@ import TransactionService from "../services/TransactionService";
 const router = express.Router();
 const transactionService = new TransactionService();
 
-router.patch("/:mobileNumber/deposite", async (req: Request, res: Response) => {
+router.patch("/:mobileNumber/deposit", async (req: Request, res: Response) => {
   try {
     const mobileNumber = req.params.mobileNumber;
     const { upiId, pin, amount, transactionType } = req.body;
-    const newBalance = await transactionService.depositeMoney(mobileNumber, upiId, pin, amount, transactionType);
+    const newBalance = await transactionService.depositMoney(mobileNumber, upiId, pin, amount, transactionType);
     // Send a successful response for depositing money
-    res.status(201).send(`₹${amount} has been deposited! Your current balance is now ₹${newBalance}`);
+    res.status(200).send(`₹${amount} have been deposited! Your current account balance is now ₹${newBalance}`);
+  } catch (error) {
+    // Return status code and error message for server-side error
+    const typedError = error as { status: number; message: string };
+    res.status(typedError.status).send(typedError.message);
+  }
+});
+
+router.patch("/:mobileNumber/withdraw", async (req: Request, res: Response) => {
+  try {
+    const mobileNumber = req.params.mobileNumber;
+    const { upiId, pin, amount, transactionType } = req.body;
+    const newBalance = await transactionService.withdrawMoney(mobileNumber, upiId, pin, amount, transactionType);
+    // Send a successful response for withdrawing money
+    res.status(200).send(`₹${amount} have been withdrawn! Your current account balance is now ₹${newBalance}`);
   } catch (error) {
     // Return status code and error message for server-side error
     const typedError = error as { status: number; message: string };
--- src/services/TransactionService.ts
+++ src/services/TransactionService.ts
@@ -1,6 +1,5 @@
-import UserDao from "../dao/UserDAO";
 import TransactionDao from "../dao/TransactionDAO";
-import {  BadRequestError } from "../exceptions/Exceptions";
+import { BadRequestError } from "../exceptions/Exceptions";
 import AccountService from "./AccountService";
 
 const transactionDao = new TransactionDao();
@@ -8,15 +7,30 @@ const accountService = new AccountService();
 
 class TransactionService {
   // Deposit money in account by UPI ID and PIN
-  async depositeMoney(mobileNumber: string, upiId: string, pin: number, amount: number, transactionType: string): Promise<number> {
+  async depositMoney(mobileNumber: string, upiId: string, pin: number, amount: number, transactionType: string): Promise<number> {
     if (!(mobileNumber && upiId && pin && amount && transactionType)) {
       throw new BadRequestError("Invalid Credential");
     }
     const balance = await accountService.checkBalance(mobileNumber, upiId, pin);
     const newBalance = balance + amount;
-    await transactionDao.updateBalance(upiId, newBalance);
     await transactionDao.createCashTransaction(upiId, amount, transactionType);
-    return newBalance
+    await transactionDao.updateBalance(upiId, newBalance);
+    return newBalance;
+  }
+
+  // Withdraw money from account by UPI ID and PIN
+  async withdrawMoney(mobileNumber: string, upiId: string, pin: number, amount: number, transactionType: string): Promise<number> {
+    if (!(mobileNumber && upiId && pin && amount && transactionType)) {
+      throw new BadRequestError("Invalid Credential");
+    }
+    const balance = await accountService.checkBalance(mobileNumber, upiId, pin);
+    const newBalance = balance - amount;
+    if (newBalance < 0) {
+      throw new BadRequestError("Insufficient funds. Your account balance is not enough to complete this withdrawal.");
+    }
+    await transactionDao.createCashTransaction(upiId, amount, transactionType);
+    await transactionDao.updateBalance(upiId, newBalance);
+    return newBalance;
   }
 }
 
